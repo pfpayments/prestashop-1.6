c0866efb9b439f211653e4870c2d3138a8895343
Nico Eigenmann <nico.eigenmann@customweb.com>
2018-11-21 11:30:59 +0100

    Release 1.0.15

---
+++ views/js/admin/general.js
@@ -0,0 +1,19 @@
+/**
+ * PostFinance Checkout Prestashop
+ *
+ * This Prestashop module enables to process payments with PostFinance Checkout (https://www.postfinance.ch).
+ *
+ * @author customweb GmbH (http://www.customweb.com/)
+ * @copyright 2017 - 2018 customweb GmbH
+ * @license http://www.apache.org/licenses/LICENSE-2.0 Apache Software License (ASL 2.0)
+ */
+jQuery(function($) {
+    
+    function movePostFinanceCheckoutManualTasks(){
+    	$("#postfinancecheckout_notifications").find("li").each(function(key, element){
+		$("#header_notifs_icon_wrapper").append(element);
+    	});
+    }
+    movePostFinanceCheckoutManualTasks();
+    
+});
