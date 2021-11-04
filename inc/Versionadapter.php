b328f470ea8cc4aac1a62c985e8a0eb4df908137
Sebastian Bossert <sebastian.bossert@wallee.com>
2021-11-04 18:30:47 +0100

    Release 1.2.10

--- inc/Versionadapter.php
+++ inc/Versionadapter.php
@@ -24,4 +24,9 @@ class PostFinanceCheckoutVersionadapter
     public static function clearCartRuleStaticCache()
     {
     }
+
+    public static function getAdminOrderTemplate()
+    {
+        return 'views/templates/admin/hook/admin_order.tpl';
+    }
 }
